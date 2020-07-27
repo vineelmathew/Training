9750d4540f87026671e2dce4142edfc455e90092
vineel Mathew <vrudrapati@IN-5CG0253HHZ>
2020-07-27 11:02:56 +0530

    added list examples and iterator

--- src/stringdemos/StringMethod.java
+++ src/stringdemos/StringMethod.java
@@ -10,7 +10,6 @@ public class StringMethod {
 		System.out.println(name.substring(4,name.length())); // printing the string from 4 to end of the string
 		System.out.println(name.equals("Mathew")); // checking whether name is equal to mathew
 		System.out.println(name.indexOf("R")); //getting the index of the letter R. if the letter doesnt exist it return -1
-		
 	}
 
 }
